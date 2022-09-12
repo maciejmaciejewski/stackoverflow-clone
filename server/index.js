f460001e2cd951c28bbfea15c81fc6ab3b255cc2
Maciej Maciejewski <maciej.maciejewski@finastra.com>
2022-09-12 09:45:33 +0200

    Try https

--- server/index.js
+++ server/index.js
@@ -1,13 +1,22 @@
 const app = require("./app");
 const mongoose = require("mongoose");
 const config = require("./config");
+const fs = require("fs")
+const https = require("https");
 
 const connect = url => {
   return mongoose.connect(url, config.db.options);
 };
 
 if (require.main === module) {
-  app.listen(config.port);
+
+  let opts = {
+      key: fs.readFileSync("key.pem"),
+      cert: fs.readFileSync("cert.pem")
+  }
+
+  https.createServer(opts, app).listen(config.port);
+
   connect(config.db.prod);
   mongoose.connection.on('error', console.log);
 }
